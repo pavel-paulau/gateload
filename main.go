e54249911f521d5bd226bef2cb7ae3df12d6411d
Traun Leyden <tleyden@couchbase.com>
2015-05-05 17:43:16 -0700

    write expvars to a file more often

--- main.go
+++ main.go
@@ -27,6 +27,12 @@ func main() {
 		log.Printf("GOMAXPROCS is set at %v", os.Getenv("GOMAXPROCS"))
 	}
 
+	// start up a goroutine that will write the expvars to a file
+	go func() {
+		<-time.After(time.Second * 60)
+		writeExpvarsToFile()
+	}()
+
 	// start up an http server, just to serve up expvars
 	go http.ListenAndServe(":9876", nil)
 
@@ -115,6 +121,7 @@ func main() {
 		wg.Wait()
 	}
 
+	// capture final snapshot of expvars
 	writeExpvarsToFile()
 
 }
